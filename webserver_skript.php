4c9671b099736bd14a2e9ca3b23a7bbbd712ab25
Darren <darrenyeo245@gmail.com>
2024-03-18 14:22:44 +0100

    lots of changes and fixes

--- webserver_skript.php
+++ webserver_skript.php
@@ -1,13 +1,34 @@
 <?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbName = 'webserver';
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-  $vorname = $_POST["vorname"];
-  $nachname = $_POST["nachname"];
-  $data = "$vorname, $nachname\n";
-  file_put_contents("daten.txt", $data, FILE_APPEND);
+//create connection
 
-  echo "Daten wurden erfolgreich gesendet!";
-
+$conn = new mysqli($servername, $username, $password, $dbName);
+//check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "INSERT INTO formular_daten (vorname, nachname, email, nachricht)
+VALUES ('" . $_POST["vorname"] . "','" . $_POST["nachname"] . "','" . $_POST["email"] . "','" . $_POST["nachricht"] . "')";
+
+$vorname = $_POST["vorname"];
+$nachname = $_POST["nachname"];
+$email = $_POST["email"];
+$nachricht = $_POST["nachricht"];
+
+$data = "$vorname, $nachname, $email, $nachricht\n";
+file_put_contents('daten.txt', $data, FILE_APPEND);
+
+if ($conn->query($sql) === TRUE) {
+  echo "Daten erfolgreich gesendet";
+}else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 
 
