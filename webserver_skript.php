c0fb5c41cc53bd97397c4c2277d01b0220ba89cd
Darren <darrenyeo245@gmail.com>
2024-03-20 08:45:07 +0100

    added comments

--- webserver_skript.php
+++ webserver_skript.php
@@ -12,20 +12,25 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+//insert data into database
 $stmt = $conn->prepare ("INSERT INTO formular_daten (vorname, nachname, email, nachricht)
 VALUES (?, ?, ?, ?)");
 
+//get data from form
 $vorname = $_POST["vorname"];
 $nachname = $_POST["nachname"];
 $email = $_POST["email"];
 $nachricht = $_POST["nachricht"];
 
+//bind parameters
 $stmt->bind_param("ssss", $vorname, $nachname, $email, $nachricht);
 $stmt->execute();
 
+//write data to file
 $data = "$vorname, $nachname, $email, $nachricht\n";
 file_put_contents('daten.txt', $data, FILE_APPEND);
 
+//check if data was inserted
 if ($stmt->affected_rows > 0) {
     echo "Daten erfolgreich gesendet";
 } else {
